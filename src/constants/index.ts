0ecc641414b4027df8e3508da5f332b5497ea558
Justin Zhang <jzha365@uwo.ca>
2021-08-13 17:26:27 -0400

    Fixed 404 and updated resume paths

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const REAL_EMAIL = "justinzhangzll@gmail.com";
 export const TEMP_EMAIL = "hirejustinzhang@gmail.com";
 export const RESUME_LINK =
-  "https://doixzan7hf4ti.cloudfront.net/other/JustinZhangResume.pdf";
+  "https://doixzan7hf4ti.cloudfront.net/resumes/JustinZhangResumePublic.pdf";
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,33 +4,33 @@ import {
   Heading,
   Image,
   Text,
-  Link as ChakraLink,
   useColorMode,
+  Container,
+  Stack,
+  Center,
 } from "@chakra-ui/react";
 import Link from "next/link";
 
 const Page404 = () => {
-  const { colorMode } = useColorMode();
-
   return (
-    <>
-      <Image src="/404 Error-pana.svg" alt="Error 404 not found Illustration" />
-
-      <Box marginY={4}>
-        <Heading textAlign="center">Page not Found.</Heading>
-
-        <Box textAlign="center" marginTop={4}>
-          <Text>{"It's Okay!"}</Text>
-          <Link href="/" passHref>
-            <Button
-              backgroundColor={colorMode === "light" ? "gray.300" : "teal.500"}
-            >
-              {"Back home"}
-            </Button>
-          </Link>
-        </Box>
-      </Box>
-    </>
+    <Container maxW="container.lg" pt="4rem">
+      <Center h="100%" minH="75vh">
+        <Stack w="100%">
+          <Image src="/404-image.svg" alt="Error 404 not found Illustration" />
+          <Box pb={16}>
+            <Stack spacing={4}>
+              <Heading>Page not found.</Heading>
+              <Text>This site only has one page!</Text>
+              <Link href="/" passHref>
+                <Button w="fit-content" colorScheme="blue">
+                  Back home
+                </Button>
+              </Link>
+            </Stack>
+          </Box>
+        </Stack>
+      </Center>
+    </Container>
   );
 };
 
