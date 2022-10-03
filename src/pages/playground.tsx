291847ca54fde3d5aefca00e40cb685b55b44d9c
Justin Zhang <justinzhangzll@gmail.com>
2022-10-02 22:03:31 -0400

    Update Meta work

---
+++ src/pages/playground.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Box, Container, Heading, Stack, Text } from "@chakra-ui/layout";
+
+const Playground = () => {
+  return (
+    <Container maxW="container.xl">
+      <Box h="4rem" />
+      <Heading>Hello</Heading>
+    </Container>
+  );
+};
+
+export default Playground;
